b8003ed41cea1d8b5d6d8bed27714eb630465d4e
JuaAcha <achilhamm.a@gmail.com>
2022-08-22 22:10:37 -0700

    menambahkan file home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+
+@section('title')
+  Home
+  @endsection
+
+  @section('content')
+  <div class="row g-2 text-center">
+    <div class="col-3">
+        <div class="p-3 border bg-light mt-3"> 10 Barang <i class="fa fa-boxes"></i> </div>
+</div>
+
+<div class="col-3">
+        <div class="p-3 border bg-light mt-3"> 15 Kategori <i class="fa fa-tag"></i> </div>
+</div>
+
+<div class="col-3">
+        <div class="p-3 border bg-light mt-3"> 5 Supplier <i class="fa fa-truck"></i> </div>
+</div>
+
+<div class="col-3">
+        <div class="p-3 border bg-light mt-3"> 100 Member <i class="fa fa-users"></i> </div>
+</div>
+
+</div>
+
+  @endsection
