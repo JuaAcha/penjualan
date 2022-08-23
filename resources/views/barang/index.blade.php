6a84d5cae4dd25357ae0368090bea260ef071c98
JuaAcha <achilhamm.a@gmail.com>
2022-08-22 21:09:15 -0700

    upload ke github

---
+++ resources/views/barang/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layout.app')
+
+@section('title')
+Barang
+@endsection
+
+@section('content')
+<div class="card mt-3">
+    <div class="card-header">
+        <div class="card-title">
+            <h5>Data Barang</h5>
+
+            <button class="btn btn-success btn-sm float-end" data-bs-toggle="modal"
+            data-bs-target="#modalTambah"><i class="fa fa-plus"></i></button>
+        </div>
+    </div>
+
+    <div class="card-body">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th>No.</th>
+        <th>Id</th>
+        <th>Nama</th>
+        <th>Harga</th>
+        <th>Stock</th>
+        <th>Supplier_id</th>
+        <th>Kategori_id</th>
+</tr>
+</thead>
+
+<tbody>
+    <tr>
+        <td>1</td>
+        <td>19</td>
+        <td>Katana Demon Slayer</td>
+        <td>5.000.000</td>
+        <td>3</td>
+        <td>14</td>
+        <td>18</td>
+        <td>
+            <a href="#" class="btn btn-warning btn-sm"> <i class="fa fa-edit"></i></a>
+            <a href="#" class="btn btn-danger btn-sm"> <i class="fa-solid fa-trash"></i></a>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modalTambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
